d4d67ba0f52eff24e725579de9202b91c9db7fbc
Taibi El Yakouti <xdweb.elyakouti@gmail.com>
2023-10-07 21:39:56 +0200

    uopate

--- Algo-py-ex/correction_py.py
+++ Algo-py-ex/correction_py.py
@@ -1,2 +1,5 @@
-name = input("Enter your name: ")
-print("Hello, " + name + "!")
+largeur = int(input("entre la largeur:"))
+longueur = int(input("entre la longueur:"))
+perimetre = (largeur + longueur) * 2
+surface = largeur * longueur
+print("le perimetre est: ", perimetre , "et la surface est: ", surface)
