1c6b02fb6279f358697fa624468e072e3ae1681b
xdweeb <xdweb.elyakouti@gmail.com>
2023-10-10 15:08:14 +0100

    rename

---
+++ 0x03 class/main.py
@@ -0,0 +1,9 @@
+from src.firstcalss import info
+
+
+xd = info()
+xd.nom = "El Yakouti"
+xd.prenom = "Taibi"
+xd.age = 20
+
+xd.affiche()
---
+++ 0x03 class/src/firstcalss.py
@@ -0,0 +1,9 @@
+class info:
+    def __init__(self) :
+        self.nom = ""
+        self.prenom = ""
+        self.age = ""
+
+    def affiche(self):
+        print(self.nom, self.prenom, self.age)
+
