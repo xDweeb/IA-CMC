f2c8b55292e41544c7b296b6840c40b06aed425c
xDweb <xdweb.elyakouti@gmail.com>
2024-04-23 07:34:04 +0100

    xdweb

--- 0x12 Machine Learning/0x05 les arbre de décision/code.py
+++ 0x12 Machine Learning/0x05 les arbre de décision/code.py
@@ -26,4 +26,19 @@ print('Accuracy Arbre x_test :', accuracyTreeTest)
 
 # Tester les paramètres max_depth et min_samples_split
 #le paramètre max_depth est un seuil sur la profondeur de l'arbre
-#le paramètre min_samples_lear donne le nomber minimal
+#le paramètre min_samples_lear donne le nomber minimal d'echantillons dans un noeud feuille.
+#Ile permettent de mettre des containers sur la construction de l'arbre
+#et donc de controler indirectement le phénomène de sur-apprentissage
+for mdepth in [1, 2, 3, 4, 10]:
+    clf = tree.DecisionTreeClassifier(max_depth=mdepth)
+    clf = clf.fit(X_train, y_train)
+    print(clf.score(X_test, y_test))
+
+for msplit in [2, 3, 5, 10, 20]:
+    clf = tree.DecisionTreeClassifier(min_samples_split=msplit)
+    clf = clf.fit(X_train, y_train)
+    print(clf.score(X_test, y_test))
+
+#Prédiction test
+modelTree.predict([[1.0,1.0,5.4,0.89]])
+
